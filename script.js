c3d591e18943558a782ca67ddf9ef01bebaf522a
Pablo <pablocifucar@gmail.com>
2022-11-16 18:16:44 +0100

    Se ha mejorado visualmente el mapa y se ha añadido que las fotos desplegadas se cierren al abrir una nueva

--- script.js
+++ script.js
@@ -2,12 +2,7 @@ let botones = document.querySelectorAll('.carta');
 let divTextoaExpandir = document.querySelectorAll('.fotos-a-expandir');
 
 
-botones.forEach((element,index) =>{
-    element.addEventListener('click',()=>{
-        divTextoaExpandir[index].classList.toggle("abrir_cerrar");
-    });
-});
-
+//FUNCION PARA SACAR LOS INDICES RESTANTES DADO UN ARRAY Y UN INDICE DE REFERENCIA
 
 a=[1,3,5,7]
 function demasindices(array,indice){
@@ -19,3 +14,14 @@ function demasindices(array,indice){
     }
     return(aux)
 }
+
+botones.forEach((element,index) =>{
+    element.addEventListener('click',()=>{
+        demasindices(botones,index).forEach((indicesrestantes)=>{
+            if(divTextoaExpandir[indicesrestantes].classList!="fotos-a-expandir"){
+                divTextoaExpandir[indicesrestantes].classList.toggle("abrir_cerrar");
+            }
+        })
+        divTextoaExpandir[index].classList.toggle("abrir_cerrar");
+    });
+});
