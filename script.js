63eb1e4f1428cfd194e88dd18fd098caea501a0e
Pablo <pablocifucar@gmail.com>
2022-11-16 18:37:25 +0100

    Se han añadido comentarios aclaratorios

--- script.js
+++ script.js
@@ -17,11 +17,15 @@ function demasindices(array,indice){
 
 botones.forEach((element,index) =>{
     element.addEventListener('click',()=>{
-        demasindices(botones,index).forEach((indicesrestantes)=>{
-            if(divTextoaExpandir[indicesrestantes].classList!="fotos-a-expandir"){
-                divTextoaExpandir[indicesrestantes].classList.toggle("abrir_cerrar");
+        
+        //ESTE FOREACH ES POR SI HAY ALGUNA FOTO YA ABIERTA PARA QUE LA CIERRE
+        demasindices(botones,index).forEach((indicesrestantes)=>{ 
+            if(divTextoaExpandir[indicesrestantes].classList=="fotos-a-expandir abrir_cerrar"){
+                divTextoaExpandir[indicesrestantes].classList = "fotos-a-expandir";
             }
         })
+
+        //LA LINEA SIGUIENTE ES PARA CAMBIAR LA CLASE DEL DIV PARA QUE SE ABRA O SE CIERRE CUANDO SE PULSE
         divTextoaExpandir[index].classList.toggle("abrir_cerrar");
     });
 });
